eb1ae58877c0cf980b63f09c8e9e3c3ec9931d73
mahmoud <mahmoudtarbolsi@gmai.com>
2024-01-17 20:47:38 +0300

    update

---
+++ main.js
@@ -0,0 +1,19 @@
+window.onload=function(){
+    const eff = document.getElementById("effect");
+
+    window.addEventListener('scroll', scrollEffect);
+
+    function scrollEffect(){
+        if(window.scrollY<150){
+            eff.style.opacity = '1';
+            eff.style.transition = '0.5s ease-in-out';
+            
+        }
+        else{
+            eff.style.opacity = '0';
+
+            eff.style.transition = '0.5s ease-in-out';
+        }
+    }
+    scrollEffect();
+}
