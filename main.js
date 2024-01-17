13a4eae9252b7d9cd57639d23592891d23ed029d
mahmoud <mahmoudtarbolsi@gmai.com>
2024-01-17 21:30:31 +0300

    update

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 window.onload=function(){
     const eff = document.getElementById("effect");
+    
 
     window.addEventListener('scroll', scrollEffect);
 
     function scrollEffect(){
-        if(window.scrollY<150){
+        if(window.scrollY<50){
             eff.style.opacity = '1';
             eff.style.transition = '0.5s ease-in-out';
             
