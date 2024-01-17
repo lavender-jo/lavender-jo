72c2c35c52de8ff10a963c8f733a2a0a07db04ff
mahmoud <mahmoudtarbolsi@gmai.com>
2024-01-18 00:33:38 +0300

    update

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ window.onload=function(){
     window.addEventListener('scroll', scrollEffect);
 
     function scrollEffect(){
-        if(window.scrollY<75){
+        if(window.scrollY<100){
             eff.style.opacity = '1';
             eff.style.transition = '0.5s ease-in-out';
             
         }
         else{
             eff.style.opacity = '0';
-
             eff.style.transition = '0.5s ease-in-out';
         }
     }
